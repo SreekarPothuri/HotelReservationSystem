3ce57be01b15c821cb9a6c85c5b33c77b161d62b
Your Name <you@example.com>
2020-11-09 19:29:44 +0530

    Added: findCheapestHotel Method

--- src/main/java/com/blz/hotelreservation/HotelReservation.java
+++ src/main/java/com/blz/hotelreservation/HotelReservation.java
@@ -1,14 +1,15 @@
 package com.blz.hotelreservation;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class HotelReservation {
 
-	private static Scanner sc = new Scanner(System.in);
 	private static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
 		
 	public ArrayList<Hotel> getHotel(){
@@ -23,15 +24,15 @@ public class HotelReservation {
 	public static int countNoOfHotels() {
 		return hotelList.size();
 	}
-
-	public static void main(String[] args) {
-		System.out.println("WELCOME TO HOTEL RESERVATION PROGRAM");
-		Hotel lakeWood=new Hotel("Lakewood",110);
-		Hotel bridgeWood=new Hotel("Bridgewood",160);
-		Hotel ridgeWood=new Hotel("Ridgewood",220);
-		hotelList.add(lakeWood);
-		hotelList.add(bridgeWood);
-		hotelList.add(ridgeWood);
-		System.out.println(hotelList);
+	
+	public static String findCheapestHotel(String startDate, String endDate) {
+		DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("dd MMM yyyy");
+		LocalDate startDateInput = LocalDate.parse(startDate, dateformat);
+		LocalDate endDateInput = LocalDate.parse(endDate, dateformat);
+		int numOfBookingDays = (int) ChronoUnit.DAYS.between(startDateInput, endDateInput) + 1;
+		Map<String, Integer> hotelNameToTotalCostMap = hotelList.stream().collect(Collectors.toMap(hotel -> hotel.getHotelName(), hotel -> hotel.getRegularRate() * numOfBookingDays));
+		String cheapestHotelName = hotelNameToTotalCostMap.keySet().stream().min((hotel1,hotel2) -> hotelNameToTotalCostMap.get(hotel1) - hotelNameToTotalCostMap.get(hotel2)).orElse(null);
+		String cheapestHotelInfo = cheapestHotelName + " Total Cost: $" + hotelNameToTotalCostMap.get(cheapestHotelName);
+		return cheapestHotelInfo;
 	}
 }
