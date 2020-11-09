1335095fd57b2db3c5068d9214ac946c23c066f0
Your Name <you@example.com>
2020-11-09 20:14:01 +0530

    Modified: The constructor and variables as per Hotel class

--- src/main/java/com/blz/hotelreservation/HotelReservation.java
+++ src/main/java/com/blz/hotelreservation/HotelReservation.java
@@ -10,29 +10,36 @@ import java.util.stream.Collectors;
 
 public class HotelReservation {
 
-	private static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
-		
-	public ArrayList<Hotel> getHotel(){
+	public static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+
+	public ArrayList<Hotel> getHotel() {
 		return hotelList;
 	}
 
-	public static void addhotel(String hotel, int price) {
-		Hotel hotels = new Hotel(hotel,price);
+	public static void addhotel(String name, int weekdayRoomRate, int weekendRoomRate) {
+		Hotel hotels = new Hotel(name, weekdayRoomRate, weekendRoomRate);
 		hotelList.add(hotels);
 	}
-	
+
 	public static int countNoOfHotels() {
 		return hotelList.size();
 	}
-	
+
 	public static String findCheapestHotel(String startDate, String endDate) {
 		DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("dd MMM yyyy");
 		LocalDate startDateInput = LocalDate.parse(startDate, dateformat);
 		LocalDate endDateInput = LocalDate.parse(endDate, dateformat);
 		int numOfBookingDays = (int) ChronoUnit.DAYS.between(startDateInput, endDateInput) + 1;
-		Map<String, Integer> hotelNameToTotalCostMap = hotelList.stream().collect(Collectors.toMap(hotel -> hotel.getHotelName(), hotel -> hotel.getRegularRate() * numOfBookingDays));
-		String cheapestHotelName = hotelNameToTotalCostMap.keySet().stream().min((hotel1,hotel2) -> hotelNameToTotalCostMap.get(hotel1) - hotelNameToTotalCostMap.get(hotel2)).orElse(null);
-		String cheapestHotelInfo = cheapestHotelName + " Total Cost: $" + hotelNameToTotalCostMap.get(cheapestHotelName);
+		Map<String, Integer> hotelNameToTotalCostMap = hotelList.stream().collect(Collectors
+				.toMap(hotel -> hotel.getHotelName(), hotel -> hotel.getWeekdayRoomRate() * numOfBookingDays));
+
+		String cheapestHotelName = hotelNameToTotalCostMap.keySet().stream()
+				.min((hotel1, hotel2) -> hotelNameToTotalCostMap.get(hotel1) - hotelNameToTotalCostMap.get(hotel2))
+				.orElse(null);
+
+		String cheapestHotelInfo = cheapestHotelName + " Total Cost: $"
+				+ hotelNameToTotalCostMap.get(cheapestHotelName);
+
 		return cheapestHotelInfo;
 	}
 }
