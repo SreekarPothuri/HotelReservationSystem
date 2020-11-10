3c966a7aa0f33fd1b9f1f1173843fab6f6ced74b
Your Name <you@example.com>
2020-11-10 16:48:53 +0530

    Added: method to findBestRatedHotel

--- src/main/java/com/blz/hotelreservation/HotelReservation.java
+++ src/main/java/com/blz/hotelreservation/HotelReservation.java
@@ -4,14 +4,19 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class HotelReservation {
 
 	private static int price;
 	public static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
 	static Map<String, Integer> map = new HashMap<>();
+	static Map<String, Integer> rent = new HashMap<>();
 
 	public ArrayList<Hotel> getHotel() {
 		return hotelList;
@@ -37,13 +42,39 @@ public class HotelReservation {
 		return date;
 	}
 
+	public static void findhotelRent(String startDate, String endDate) {
+		LocalDate startDateInput = convertStringToDate(startDate);
+		LocalDate endDateInput = convertStringToDate(endDate);
+		Integer hotelRent;
+
+		addhotel("LakeWood", 110, 90, 3);
+		addhotel("BridgeWood", 150, 50, 4);
+		addhotel("RidgeWood", 220, 150, 5);
+
+		for (Hotel hotelDetail : hotelList) {
+			LocalDate startDay = startDateInput;
+			LocalDate endDay = endDateInput.plusDays(1);
+			hotelRent = 0;
+			while (!(startDay.equals(endDay))) {
+				int day = startDay.getDayOfWeek().getValue();
+				if (day == 6 || day == 7) {
+					hotelRent = hotelRent + hotelDetail.getWeekendRoomRate();
+				} else {
+					hotelRent = hotelRent + hotelDetail.getWeekdayRoomRate();
+				}
+				startDay = startDay.plusDays(1);
+			}
+			rent.put(hotelDetail.getHotelName(), hotelRent);
+		}
+	}
+
 	public static ArrayList<String> findCheapestHotel(String startDate, String endDate) {
 		LocalDate startDateInput = convertStringToDate(startDate);
 		LocalDate endDateInput = convertStringToDate(endDate);
 
-		hotelList.add(new Hotel("Lakewood", 110, 90, 3));
-		hotelList.add(new Hotel("Bridgewood", 150, 50, 4));
-		hotelList.add(new Hotel("Ridgewood", 220, 150, 5));
+		addhotel("LakeWood", 110, 90, 3);
+		addhotel("BridgeWood", 150, 50, 4);
+		addhotel("RidgeWood", 220, 150, 5);
 		ArrayList<String> cheapestHotelNameList = new ArrayList<>();
 
 		int minRate = Integer.MAX_VALUE;
@@ -80,5 +111,21 @@ public class HotelReservation {
 		}
 		return cheapestBestRatedHotel.getKey() + ", Rating: " + cheapestBestRatedHotel.getValue()
 				+ " and Total Rates: $" + price;
+	}
+
+	public String findBestRatedHotel(String startDate, String endDate) {
+		findhotelRent(startDate, endDate);
+		addhotel("LakeWood", 110, 90, 3);
+		addhotel("BridgeWood", 150, 50, 4);
+		addhotel("RidgeWood", 220, 150, 5);
+		int bestRating = 0;
+		Hotel BestRatedHotel = null;
+		for (Hotel hotelDetail : hotelList) {
+			if (hotelDetail.getRate() > bestRating) {
+				bestRating = hotelDetail.getRate();
+				BestRatedHotel = hotelDetail;
+			}
+		}
+		return BestRatedHotel.getHotelName() + " & Total Rates: $" + rent.get(BestRatedHotel.getHotelName());
 	}
 }
