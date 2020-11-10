99ffd938669ead4669913731f404287148fd9603
Your Name <you@example.com>
2020-11-10 08:13:02 +0530

    Added: Test case to check findCheapestHotel method

--- src/test/java/com/blz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/blz/hotelreservation/HotelReservationTest.java
@@ -13,42 +13,12 @@ public class HotelReservationTest {
 
 	@Test
 	public void givendetailsOf3Hotels_WhenAddedToHotelList_SizeOfListIs3() {
-
-		HotelReservation.addhotel("Lakewood", 110, 90);
-		HotelReservation.addhotel("Bridgewood", 160, 60);
-		HotelReservation.addhotel("Ridgewood", 220, 150);
-		Assert.assertEquals(9, HotelReservation.countNoOfHotels());
+		Assert.assertEquals(3, HotelReservation.countNoOfHotels());
 	}
 
 	@Test
 	public void givenDetailsOf3Hotels_InAGivenDataRage_shouldReturnCheapesthotel() {
-
-		HotelReservation.addhotel("Lakewood", 110, 90);
-		HotelReservation.addhotel("Bridgewood", 160, 60);
-		HotelReservation.addhotel("Ridgewood", 220, 150);
-		String cheapestHotelInfo = HotelReservation.findCheapestHotel("10 Sep 2020", "11 Sep 2020");
+		String cheapestHotelInfo = HotelReservation.findCheapestHotel("10Sep2020", "11Sep2020");
 		Assert.assertEquals("Lakewood Total Cost: $220", cheapestHotelInfo);
-	}
-
-	@Test
-	public void givenDetailsOf3Hotels_WhenWeekdayAndWeekendRatesAdded_ShouldReturnThoseRates() {
-
-		HotelReservation.addhotel("Lakewood", 110, 90);
-		HotelReservation.addhotel("Bridgewood", 160, 60);
-		HotelReservation.addhotel("Ridgewood", 220, 150);
-		List<Integer> weekendRoomRates = new ArrayList<>();
-		List<Integer> weekdayRoomRates = new ArrayList<>();
-		HotelReservation.hotelList.stream().forEach(hotelDetails -> {
-			weekendRoomRates.add(hotelDetails.getWeekendRoomRate());
-			weekdayRoomRates.add(hotelDetails.getWeekdayRoomRate());
-			String cheapestHotelInfo = HotelReservation.findCheapestHotel("10 Sep 2020", "11 Sep 2020");
-			Assert.assertEquals("Lakewood Total Cost: $220", cheapestHotelInfo);
-		});
-		Assert.assertEquals(110, (int) weekdayRoomRates.get(0));
-		Assert.assertEquals(160, (int) weekdayRoomRates.get(1));
-		Assert.assertEquals(220, (int) weekdayRoomRates.get(2));
-		Assert.assertEquals(90, (int) weekendRoomRates.get(0));
-		Assert.assertEquals(60, (int) weekendRoomRates.get(1));
-		Assert.assertEquals(150, (int) weekendRoomRates.get(2));
 	}
 }
