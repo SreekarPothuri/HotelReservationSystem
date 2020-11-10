85f42f6ddc128b9ba088959834aa9a7aa266badf
Your Name <you@example.com>
2020-11-10 09:00:53 +0530

    Modified: Test case to find cheapest hotel

--- src/test/java/com/blz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/blz/hotelreservation/HotelReservationTest.java
@@ -1,18 +1,13 @@
 package com.blz.hotelreservation;
 
-import static org.junit.Assert.*;
-
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import com.blz.hotelreservation.*;
 
 public class HotelReservationTest {
-	
+
 	private static HotelReservation obj;
 
 	@BeforeClass
@@ -20,11 +15,6 @@ public class HotelReservationTest {
 		obj = new HotelReservation();
 	}
 
-	@Test
-	public void addHotelDetailsInHotelReservationSystem() throws ParseException {
-		obj.addRatesForAllDays();
-	}
-	
 	@Test
 	public void givenHotelsFindTheCheapestOne() throws ParseException {
 		Object[] resultHotelName = obj.findCheapestHotel("11Sep2020", "12Sep2020").toArray();
