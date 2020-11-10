f54db8119ea69327d5619fe658ecb5b6ad82d108
Your Name <you@example.com>
2020-11-10 10:08:33 +0530

    Added: rate variable to add ratings as parameter to hotel

--- src/main/java/com/blz/hotelreservation/Hotel.java
+++ src/main/java/com/blz/hotelreservation/Hotel.java
@@ -5,11 +5,13 @@ public class Hotel {
 	private String hotelName;
 	private int weekdayRoomRate;
 	private int weekendRoomRate;
+	private int rate;
 
-	public Hotel(String hotelName, int weekDayRoomRate, int weekendRoomRate) {
+	public Hotel(String hotelName, int weekDayRoomRate, int weekendRoomRate, int rate) {
 		this.hotelName = hotelName;
 		this.weekdayRoomRate = weekDayRoomRate;
 		this.weekendRoomRate = weekendRoomRate;
+		this.rate = rate;
 	}
 
 	public String getHotelName() {
@@ -36,9 +38,17 @@ public class Hotel {
 		this.weekendRoomRate = weekendRoomRate;
 	}
 
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
 	@Override
 	public String toString() {
 		return "HotelDetails [Hotel Name: " + hotelName + "  WeekdayRoomRate: " + weekdayRoomRate + " WeekendRoomRate: "
-				+ weekendRoomRate + "]";
+				+ weekendRoomRate + " Rating: " + rate + "]";
 	}
 }
