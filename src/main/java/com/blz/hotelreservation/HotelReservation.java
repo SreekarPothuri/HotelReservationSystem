db5e70cc3d731b5906349bb1f39f892675aadce5
Your Name <you@example.com>
2020-11-10 08:45:02 +0530

    Added: weekday and weekend rates into constructor

--- src/main/java/com/blz/hotelreservation/HotelReservation.java
+++ src/main/java/com/blz/hotelreservation/HotelReservation.java
@@ -2,22 +2,20 @@ package com.blz.hotelreservation;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.Comparator;
 
 public class HotelReservation {
-
 	public static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
 
 	public ArrayList<Hotel> getHotel() {
 		return hotelList;
 	}
 
-	public static void addhotel(String name, int weekdayRoomRate, int weekendRoomRate) {
-		Hotel hotels = new Hotel(name, weekdayRoomRate, weekendRoomRate);
+	public static void addhotel(String hotel, int weekDayPrice, int weekEndPrice) {
+		Hotel hotels = new Hotel(hotel, weekDayPrice, weekEndPrice);
 		hotelList.add(hotels);
 	}
 
@@ -25,21 +23,36 @@ public class HotelReservation {
 		return hotelList.size();
 	}
 
-	public static String findCheapestHotel(String startDate, String endDate) {
-		DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("dd MMM yyyy");
-		LocalDate startDateInput = LocalDate.parse(startDate, dateformat);
-		LocalDate endDateInput = LocalDate.parse(endDate, dateformat);
-		int numOfBookingDays = (int) ChronoUnit.DAYS.between(startDateInput, endDateInput) + 1;
-		Map<String, Integer> hotelNameToTotalCostMap = hotelList.stream().collect(Collectors
-				.toMap(hotel -> hotel.getHotelName(), hotel -> hotel.getWeekdayRoomRate() * numOfBookingDays));
-
-		String cheapestHotelName = hotelNameToTotalCostMap.keySet().stream()
-				.min((hotel1, hotel2) -> hotelNameToTotalCostMap.get(hotel1) - hotelNameToTotalCostMap.get(hotel2))
-				.orElse(null);
-
-		String cheapestHotelInfo = cheapestHotelName + " Total Cost: $"
-				+ hotelNameToTotalCostMap.get(cheapestHotelName);
-
-		return cheapestHotelInfo;
+	public static LocalDate convertStringToDate(String dateString) {
+		LocalDate date = null;
+		DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		try {
+			date = LocalDate.parse(dateString, dateTimeFormat);
+		} catch (DateTimeParseException e) {
+			e.printStackTrace();
+		}
+		return date;
 	}
+
+	public static String findCheapestHotel(String startDate, String endDate) {
+
+		LocalDate startDateInput = convertStringToDate(startDate);
+		LocalDate endDateInput = convertStringToDate(endDate);
+
+		int numOfBookingDays = (int) ChronoUnit.DAYS.between(startDateInput, endDateInput) + 1;
+		
+		hotelList.add(new Hotel("Lakewood", 110, 90));
+		hotelList.add(new Hotel("Bridgewood", 160, 60));
+		hotelList.add(new Hotel("Ridgewood", 220, 150));
+		
+		for(int hotel = 0; hotel < hotelList.size(); hotel++) {
+			int newRate = hotelList.get(hotel).getWeekdayRoomRate() * (numOfBookingDays);
+			hotelList.get(hotel).setWeekdayRoomRate(newRate);
+		}
+		String hotelName = hotelList.stream().min(Comparator.comparing(Hotel::getWeekdayRoomRate)).get()
+				.getHotelName();
+		int regularRate = hotelList.stream().min(Comparator.comparing(Hotel::getWeekdayRoomRate)).get()
+				.getWeekdayRoomRate();
+		return hotelName + " Total Cost: $" + regularRate;
+		}
 }
